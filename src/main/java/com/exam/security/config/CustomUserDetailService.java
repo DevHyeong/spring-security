407c5d4ae2ecfb0c3b8aeb243d2c7aa27f017ff5
chojh3123 <chojh3123@gmail.com>
2023-03-02 23:07:58 +0900

    UserDetails, UserDetailsService 적용

---
+++ src/main/java/com/exam/security/config/CustomUserDetailService.java
@@ -0,0 +1,37 @@
+package com.exam.security.config;
+
+import com.exam.security.domain.follow.entity.Follow;
+import com.exam.security.domain.follow.repository.FollowRepository;
+import com.exam.security.domain.user.entity.User;
+import com.exam.security.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final FollowRepository followRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(()-> new UsernameNotFoundException(username));
+
+        List<Follow> follows = followRepository.findByFromUserId(user.getId());
+        List<SimpleGrantedAuthority> grantedAuthorities = follows.stream()
+                .map(e-> new SimpleGrantedAuthority(e.getToUser().getId() + "/" + e.getRole()))
+                .collect(Collectors.toList());
+
+        return new SecuredUser(user, grantedAuthorities);
+    }
+}
---
+++ src/main/java/com/exam/security/config/SecuredUser.java
@@ -0,0 +1,61 @@
+package com.exam.security.config;
+
+import com.exam.security.domain.user.entity.User;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+public class SecuredUser implements UserDetails, Serializable {
+
+    @Getter
+    @Setter
+    private User user;
+
+    private List<SimpleGrantedAuthority> grantedAuthorities;
+
+    public SecuredUser(User user, List<SimpleGrantedAuthority> grantedAuthorities){
+        this.user = user;
+        this.grantedAuthorities = grantedAuthorities;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.grantedAuthorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
