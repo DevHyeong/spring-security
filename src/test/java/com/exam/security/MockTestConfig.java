32a3512525b27d35fd4563f69dff49580df0ac1d
chojh3123 <chojh3123@gmail.com>
2023-03-03 22:39:51 +0900

    @Transactional 추가

--- src/test/java/com/exam/security/MockTestConfig.java
+++ src/test/java/com/exam/security/MockTestConfig.java
@@ -4,11 +4,13 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.MOCK
 )
 @AutoConfigureMockMvc
+@Transactional
 public class MockTestConfig {
 }
